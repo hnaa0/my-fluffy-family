73a189956352b0b46442420a5223d68fa4602503
hnaa0 <hnaa12@daum.net>
2024-01-26 16:02:04 +0900

    💄Add AnimlaCardInfo Page

---
+++ src/Pages/AnimalCardInfo.tsx
@@ -0,0 +1,260 @@
+import styled from "styled-components";
+import sample from "../assets/images/infoimage.jpg";
+
+export default function AnimalCardInfo() {
+  return (
+    <>
+      <Container>
+        <Mailtitle>라도 | 마포센터</Mailtitle>
+        <InfoGroup>
+          <ImgBox>
+            <MainImg src={sample} />
+            <NextImgGroup>
+              <div>
+                <img src={sample} />
+              </div>
+              <div>
+                <img src={sample} />
+              </div>
+              <div>
+                <img src={sample} />
+              </div>
+              <div>
+                <img src={sample} />
+              </div>
+              <div>
+                <img src={sample} />
+              </div>
+              <div>
+                <img src={sample} />
+              </div>
+              <div>
+                <img src={sample} />
+              </div>
+              <div>
+                <img src={sample} />
+              </div>
+              <div>
+                <img src={sample} />
+              </div>
+            </NextImgGroup>
+          </ImgBox>
+          <TextGroup>
+            <InfoBox>
+              <InfoTitle>기본정보</InfoTitle>
+              <InfoTable>
+                <tbody>
+                  <tr>
+                    <td>입소 날짜</td>
+                    <td>2023.04.20</td>
+                  </tr>
+                  <tr>
+                    <td>종</td>
+                    <td>DOG</td>
+                  </tr>
+                  <tr>
+                    <td>품종</td>
+                    <td>믹스</td>
+                  </tr>
+                  <tr>
+                    <td>성별</td>
+                    <td>남</td>
+                  </tr>
+                  <tr>
+                    <td>나이</td>
+                    <td>0(세) 9(개월)</td>
+                  </tr>
+                  <tr>
+                    <td>체중</td>
+                    <td>10.9kg</td>
+                  </tr>
+                </tbody>
+              </InfoTable>
+            </InfoBox>
+            <BtnBox>
+              <button>방문예약</button>
+              <button>입양절차안내</button>
+            </BtnBox>
+          </TextGroup>
+        </InfoGroup>
+        <DescGroup>
+          <DescTitle>상세설명</DescTitle>
+          <DescBox>
+            <iframe
+              width="600"
+              height="300"
+              src="https://www.youtube.com/embed/rYyirwLh-BU?si=lSZPqTpDcTEQSgGx"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+            <DescTxt>
+              이제는 견생 6개월차에 접어든 ~ 그중 첫째(?) 리도를 소개합니다. 네
+              아빠가 누구니? 물어보고 싶은 단지를 아주 살짝만 닮은 흰양말은 신고
+              가슴에는 큰 흰하트를 지닌 아기 강아지예요.! 사람을 좋아하고
+              호기심도 많지만, 엄마를 닮아서인지 살짝 겁쟁이기도 해요! 유치가
+              나는 중이라서 잇몸이 근질 근질~ 센터의 기물을 씹고 맛보고 쪽쪽대며
+              아주 난리예요~ ㅎㅎㅎㅎㅎ 밥도 잘 먹고 패드 배변도 어느정도 잘
+              가립니다. 부르는 소리에 쫄쫄쫄 다가오기도 하는 아기 강아지! 하얀
+              백지같은 어린 강아지입니다. 성견이 됐을 때 기본 10kg는 넘을 수도
+              있어요. 크기가 커질까봐 걱정이 되거나, 키우면서 집안 물건들을 긁고
+              찢고 뜯고 하는게 싫어 가둬두실 분, 기본 지시어 훈련부터 산책 훈련,
+              사회화 훈련 등 훈련에 관심없으신 분, 중성화 수술을 비동의 하시는
+              분, 등 이렇게 어린 강아지는 정말 정말 손이 많이 갑니다. 마치 1~2살
+              자녀를 다시 양육하는 심정으로 관리하셔야 돼요. 윗 사항들이 문제가
+              되거나 걱정이 크신 분들은 입양을 추천드리지 않아요. 10KG! 20KG!
+              되도 상관없이 많이 먹고 쑥쑥 자라나 건강하게 잘 지내길 바라는
+              가족. 집에 사회성이 좋은 강아지가 있는 반려 가족. 어린 강아지
+              특성상 손이 많이 가기 때문에 1인 가족보다는 여러 구성원이 있는
+              다인가족이 적합합니다. 처음 반려견을 맞이하거나 영유아 이상의
+              자녀가 있는 가정등도 적합해요! 사랑스러운 작은 강아지 리도에게
+              알록 달록 즐겁고 신나는 세상을 보여주실 평생가족을 기다립니다.
+              소심한 성격으로 차분한 강아지 입니다, 똑똑해서 2개월차에 자기
+              이름도 알아듣고 패드배변도 너무 너무 완벽하게 잘해요. 순하고
+              영리한 한국 똥강아지~리도~ 심장에 하얀 하트가 자라면서 찌부(?)되고
+              있는건 안비밀! :) 다른 강아지들과도 너무 너무 잘 지내고 아직도
+              작은 아기 강아지인줄 아는 무릎에 폭 안기고 싶어하는 꼬꼬마 강아지
+              리도~ 고냥이 랑이 목소리에 무섭다고 깽깽깽 하는 겁쟁이 리도~
+              패드배변도 잘하고 똑똑하답니다^^ 센터에서 자랐기 때문에 자동차 ,
+              자전거, 다른 생활 소음에 취약합니다. (강아지 짖는 소리는 강해요~
+              ㅎㅎㅎㅎ) 입양후 충분히 가정생활에 적응 할 수 있도록 둔감화 훈련이
+              필요합니다. (산책 훈련 필요!!~~!~)
+            </DescTxt>
+          </DescBox>
+        </DescGroup>
+      </Container>
+    </>
+  );
+}
+
+const SubTitle = styled.h3`
+  display: block;
+  font-weight: bold;
+  margin-bottom: 24px;
+`;
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 100px;
+  letter-spacing: -0.3px;
+`;
+
+const Mailtitle = styled.h2`
+  font-size: 36px;
+  font-weight: bold;
+  margin-bottom: 52px;
+`;
+
+const InfoGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 1200px;
+  margin-bottom: 100px;
+`;
+
+const ImgBox = styled.div`
+  width: 600px;
+`;
+
+const MainImg = styled.img`
+  width: 100%;
+  height: 600px;
+  object-fit: cover;
+`;
+
+const NextImgGroup = styled.div`
+  display: flex;
+  margin-top: 8px;
+  cursor: pointer;
+
+  div {
+    width: 50px;
+    height: 50px;
+    margin-right: 4px;
+  }
+
+  div > img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const TextGroup = styled.div`
+  flex-shrink: 0;
+  height: 100%;
+`;
+
+const InfoBox = styled.div`
+  width: 500px;
+  margin: 28px 0;
+`;
+
+const InfoTitle = styled(SubTitle)`
+  font-size: 18px;
+`;
+
+const InfoTable = styled.table`
+  width: 100%;
+  padding: 20px;
+  margin-bottom: 12px;
+  border-top: 1px solid var(--color-gray-light);
+
+  td {
+    border-bottom: 1px solid var(--color-gray-light);
+    padding: 24px;
+  }
+
+  tr td:first-of-type {
+    font-size: 14px;
+    font-weight: bold;
+    color: var(--color-gray-normal);
+  }
+`;
+
+const BtnBox = styled.div`
+  display: flex;
+  justify-content: end;
+  width: 100%;
+
+  button {
+    width: 120px;
+    height: 40px;
+    text-align: center;
+    font-size: 14px;
+    font-weight: bold;
+    letter-spacing: -0.5px;
+    margin-left: 12px;
+    border-radius: 20px;
+    background-color: var(--color-yellow-normal);
+    transition: all 0.2s;
+
+    &:hover {
+      background-color: var(--color-yellow-light);
+    }
+  }
+`;
+
+const DescGroup = styled.div`
+  width: 1200px;
+`;
+
+const DescTitle = styled(SubTitle)`
+  font-size: 24px;
+  padding: 12px;
+  border-bottom: 1px solid var(--color-gray-light);
+`;
+
+const DescBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 58px 0;
+`;
+
+const DescTxt = styled.p`
+  line-height: 1.8;
+  margin-top: 48px;
+`;
