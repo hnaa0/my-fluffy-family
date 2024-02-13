604957608bb0d8f0907ffe2ace7afb4046670a30
hnaa0 <hnaa12@daum.net>
2024-02-13 16:06:55 +0900

    💄Add ScrollToTop component

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
+import ScrollToTop from "./components/ScrollToTop.tsx";
 import "./styles/fonts.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </Provider>
