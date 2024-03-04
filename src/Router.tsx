a1a93622e44a224e878edd619eddd8c43ad30b72
hnaa0 <hnaa12@daum.net>
2024-03-04 17:00:02 +0900

    ✨Update Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -16,7 +16,10 @@ export default function AppRouter() {
         <Route path="/info/centerinfo" element={<CenterInfo />} />
         <Route path="/info/faq" element={<FAQ />} />
         <Route path="/findfamily/animals" element={<Animals />} />
-        <Route path="/animals/:animalNO" element={<AnimalCardInfo />} />
+        <Route
+          path="/findfamily/animals/:animalNO"
+          element={<AnimalCardInfo />}
+        />
         <Route path="/findfamily/adoptprocess" element={<AdoptProcess />} />
         <Route path="/findfamily/fosterprogram" element={<Fosterprogram />} />
         <Route path="/playground" element={<Playground />} />
